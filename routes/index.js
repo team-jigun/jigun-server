dc11edf5c8c4ab8373d5098ceb63b7081d17d421
강현구 <fred16157@gmail.com>
2021-03-09 16:21:54 +0900

    페이지 버그 수정

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+    if(!req.query.success) req.query.success = false
+    if(!req.query.failed) req.query.failed = false
     res.render('index', {success: req.query.success, failed: req.query.failed})
 });
 
